4c437eca8aafa7f1a5cc4dd366bb37dcee1e2057
Chanawut <chanawut.w@ku.th>
2024-03-02 23:57:45 +0700

    add dropdown navbar

--- src/page/component/Navbar.js
+++ src/page/component/Navbar.js
@@ -1,32 +1,122 @@
-import React from "react";
 import { Link } from "react-router-dom";
 import './Navbar.css';
+import {
+    Ripple,
+    initTE,
+} from "tw-elements";
+import React, { useState } from 'react';
+
+initTE({ Ripple });
 
 function Navbar() {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleDropdown = () => {
+        setIsOpen(!isOpen);
+    };
+
     return (
         <nav class="sticky bg-gradient-to-r from-transparent
-        via-white to-transparent background-animate border-b lg:py-3 md:py-2 py-0 top-0 z-50">
+        via-white to-transparent background-animate border-b lg:py-1 md:py-0 py-0 top-0 z-50">
             <div class="flex justify-between items-center">
-                <div class="font-semibold ml-10 text-xl">
-                    
-                </div>
+
                 <div class="mx-auto flex space-x-4 ">
-                    <Link to="/" class="inline-flex mx-auto text-white rounded-lg">
+                    <Link to="/" class="ml-8 inline-flex mx-auto text-white rounded-lg">
                         <div class="font-semibold lg:text-4xl md:text-3xl text-2xl ">
                             Ara Ara
                         </div>
                     </Link>
                 </div>
-                <Link to="/project" class=" text-white mr-6 rounded-lg hover:animate-pulse">
-                    <div class="font-semibold lg:text-4xl md:text-3xl text-xl" >
+                <div class="relative mr-2">
+                    <button onClick={toggleDropdown} class="px-4 py-2 bg-zinc-800 text-white rounded-md">
                         <svg
-                            class="lg:w-10 lg:h-10 md:w-8 md:h-8 w-6 h-6 mx-1 text-white dark:text-white group-hover:text-gray-900 dark:group-hover:text-white"
+                            class="lg:w-8 lg:h-8 md:w-6 md:h-6 w-4 h-4  text-white dark:text-white group-hover:text-gray-900 dark:group-hover:text-white"
                             aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 18">
-                            <path
-                                d="M14 2a3.963 3.963 0 0 0-1.4.267 6.439 6.439 0 0 1-1.331 6.638A4 4 0 1 0 14 2Zm1 9h-1.264A6.957 6.957 0 0 1 15 15v2a2.97 2.97 0 0 1-.184 1H19a1 1 0 0 0 1-1v-1a5.006 5.006 0 0 0-5-5ZM6.5 9a4.5 4.5 0 1 0 0-9 4.5 4.5 0 0 0 0 9ZM8 10H5a5.006 5.006 0 0 0-5 5v2a1 1 0 0 0 1 1h11a1 1 0 0 0 1-1v-2a5.006 5.006 0 0 0-5-5Z" />
+                            <path d="M14 2a3.963 3.963 0 0 0-1.4.267 6.439 6.439 0 0 1-1.331 6.638A4 4 0 1 0 14 2Zm1 9h-1.264A6.957 6.957 0 0 1 15 15v2a2.97 2.97 0 0 1-.184 1H19a1 1 0 0 0 1-1v-1a5.006 5.006 0 0 0-5-5ZM6.5 9a4.5 4.5 0 1 0 0-9 4.5 4.5 0 0 0 0 9ZM8 10H5a5.006 5.006 0 0 0-5 5v2a1 1 0 0 0 1 1h11a1 1 0 0 0 1-1v-2a5.006 5.006 0 0 0-5-5Z" />
                         </svg>
+                    </button>
+
+                    <div
+                        className={`absolute left-0 mt-2 bg-white shadow-md rounded-md transition-all duration-300 ${isOpen ? 'transform scale-y-100' : 'transform scale-y-0 -translate-y-2'
+                            }`}>
+                    <div class="absolute left-0 mt-2 bg-white shadow-md rounded-md">
+                        <div class="mt-2 mx-2">
+                            <Link to="https://www.facebook.com/profile.php?id=100001931479041">
+                                <button
+                                    type="button"
+                                    data-te-ripple-init
+                                    data-te-ripple-color="light"
+                                    class="mb-2 inline-block bg-blue-500 rounded px-3 py-2.5 text-xs font-medium uppercase leading-normal text-white shadow-md transition duration-150 ease-in-out hover:shadow-lg ">
+                                    <svg
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        class="h-4 w-4"
+                                        fill="currentColor"
+                                        viewBox="0 0 24 24">
+                                        <path
+                                            d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z" />
+                                    </svg>
+                                </button>
+                            </Link>
+                        </div>
+                        <div class=" mx-2">
+                            <Link to="https://www.instagram.com/arararaarararararararararara/">
+                                <button
+                                    type="button"
+                                    data-te-ripple-init
+                                    data-te-ripple-color="light"
+                                    class="mb-2 inline-block bg-pink-500 rounded px-3 py-2.5 text-xs font-medium uppercase leading-normal text-white shadow-md transition duration-150 ease-in-out hover:shadow-lg ">
+                                    <svg
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        class="h-4 w-4"
+                                        fill="currentColor"
+                                        viewBox="0 0 24 24">
+                                        <path
+                                            d="M12 2.163c3.204 0 3.584.012 4.85.07 3.252.148 4.771 1.691 4.919 4.919.058 1.265.069 1.645.069 4.849 0 3.205-.012 3.584-.069 4.849-.149 3.225-1.664 4.771-4.919 4.919-1.266.058-1.644.07-4.85.07-3.204 0-3.584-.012-4.849-.07-3.26-.149-4.771-1.699-4.919-4.92-.058-1.265-.07-1.644-.07-4.849 0-3.204.013-3.583.07-4.849.149-3.227 1.664-4.771 4.919-4.919 1.266-.057 1.645-.069 4.849-.069zm0-2.163c-3.259 0-3.667.014-4.947.072-4.358.2-6.78 2.618-6.98 6.98-.059 1.281-.073 1.689-.073 4.948 0 3.259.014 3.668.072 4.948.2 4.358 2.618 6.78 6.98 6.98 1.281.058 1.689.072 4.948.072 3.259 0 3.668-.014 4.948-.072 4.354-.2 6.782-2.618 6.979-6.98.059-1.28.073-1.689.073-4.948 0-3.259-.014-3.667-.072-4.947-.196-4.354-2.617-6.78-6.979-6.98-1.281-.059-1.69-.073-4.949-.073zm0 5.838c-3.403 0-6.162 2.759-6.162 6.162s2.759 6.163 6.162 6.163 6.162-2.759 6.162-6.163c0-3.403-2.759-6.162-6.162-6.162zm0 10.162c-2.209 0-4-1.79-4-4 0-2.209 1.791-4 4-4s4 1.791 4 4c0 2.21-1.791 4-4 4zm6.406-11.845c-.796 0-1.441.645-1.441 1.44s.645 1.44 1.441 1.44c.795 0 1.439-.645 1.439-1.44s-.644-1.44-1.439-1.44z" />
+                                    </svg>
+                                </button>
+                            </Link>
+                        </div>
+                        <div class="mx-2">
+                            <Link to="https://www.instagram.com/arararaarararararararararara/">
+                                <button
+                                    type="button"
+                                    data-te-ripple-init
+                                    data-te-ripple-color="light"
+                                    class="mb-2 inline-block bg-red-600 rounded px-3 py-2.5 text-xs font-medium uppercase leading-normal text-white shadow-md transition duration-150 ease-in-out hover:shadow-lg ">
+                                    <svg
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        class="h-4 w-4"
+                                        fill="currentColor"
+                                        viewBox="0 0 24 24">
+                                        <path
+                                            d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z" />
+                                    </svg>
+                                </button>
+                            </Link>
+                        </div>
+                        <div class="mx-2">
+                            <Link to="https://www.instagram.com/arararaarararararararararara/">
+                                <button
+                                    type="button"
+                                    data-te-ripple-init
+                                    data-te-ripple-color="light"
+                                    class="mb-2 inline-block bg-red-600 rounded px-3 py-2.5 text-xs font-medium uppercase leading-normal text-white shadow-md transition duration-150 ease-in-out hover:shadow-lg ">
+                                    <svg
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        class="h-4 w-4"
+                                        fill="currentColor"
+                                        viewBox="0 0 24 24">
+                                        <path
+                                            d="M12 0c-6.627 0-12 5.372-12 12 0 5.084 3.163 9.426 7.627 11.174-.105-.949-.2-2.405.042-3.441.218-.937 1.407-5.965 1.407-5.965s-.359-.719-.359-1.782c0-1.668.967-2.914 2.171-2.914 1.023 0 1.518.769 1.518 1.69 0 1.029-.655 2.568-.994 3.995-.283 1.194.599 2.169 1.777 2.169 2.133 0 3.772-2.249 3.772-5.495 0-2.873-2.064-4.882-5.012-4.882-3.414 0-5.418 2.561-5.418 5.207 0 1.031.397 2.138.893 2.738.098.119.112.224.083.345l-.333 1.36c-.053.22-.174.267-.402.161-1.499-.698-2.436-2.889-2.436-4.649 0-3.785 2.75-7.262 7.929-7.262 4.163 0 7.398 2.967 7.398 6.931 0 4.136-2.607 7.464-6.227 7.464-1.216 0-2.359-.631-2.75-1.378l-.748 2.853c-.271 1.043-1.002 2.35-1.492 3.146 1.124.347 2.317.535 3.554.535 6.627 0 12-5.373 12-12 0-6.628-5.373-12-12-12z"
+                                            fill-rule="evenodd"
+                                            clip-rule="evenodd" />
+                                    </svg>
+                                </button>
+                            </Link>
+                        </div>
                     </div>
-                </Link>
+                    </div>
+                </div>
             </div>
 
         </nav >
